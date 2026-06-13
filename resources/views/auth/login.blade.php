9519dc191ab1bef216cd6cb34c535ba5767feea7
teknoumut <umut87732@gmail.com>
2026-06-13 20:42:15 +0300

    login sayfasina logo eklendi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -438,10 +438,7 @@
             </div>
             <div class="content">
                 <div class="brand-icon">
-                    <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-                        <polyline points="22,6 12,13 2,6" />
-                    </svg>
+                    <img src="{{ asset('images/logo.svg') }}" alt="senin 💝 davetiyen" class="w-20 h-20 object-contain mx-auto">
                 </div>
                 <h1>senin 💝 davetiyen</h1>
                 
